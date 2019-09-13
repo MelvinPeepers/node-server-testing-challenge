97c9e24cd0b09dbc1dad70dbf24148735036d409
MelvinPeepers <peepersm@gmail.com>
2019-09-12 22:32:07 -0400

    setup server.js and server.spec.js

--- api/server.js
+++ api/server.js
@@ -1,7 +1,9 @@
-const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+require("dotenv").config();
+
+const Avengers = require("../avengers/avengers-Model.js");
 
 const server = express();
 
@@ -9,8 +11,18 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
-server.get('/', (req, res) => {
-  res.send("Alive and Kicking!");
+server.get("/", (req, res) => {
+  res.status(200).json({ api: "up" });
 });
-  
+
+server.get("/avengers", (req, res) => {
+  Avengers.getAll()
+    .then(avengers => {
+      res.status(200).json(avengers);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
 module.exports = server;
